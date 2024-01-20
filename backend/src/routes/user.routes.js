8c55c49636de209a3ee175481d659b847976bf3f
Prabhat2912 <pk993105@gmail.com>
2024-01-20 22:22:08 +0530

    some tests

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -33,8 +33,6 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-//secured  routes
-
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
